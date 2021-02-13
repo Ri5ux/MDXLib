dbc5dca73806b09d33a52b5ffcd25e8772235c2d
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-02-13 12:50:26 -0500

    Ported IWorldSaveHandler
    to 1.16.5

--- src/main/java/com/asx/mdx/lib/world/storage/IWorldSaveHandler.java
+++ src/main/java/com/asx/mdx/lib/world/storage/IWorldSaveHandler.java
@@ -1,11 +1,11 @@
 package com.asx.mdx.lib.world.storage;
 
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.world.World;
 
 public interface IWorldSaveHandler
 {
-    public boolean saveData(World world, NBTTagCompound nbt);
+    public boolean saveData(World world, CompoundNBT nbt);
     
-    public boolean loadData(World world, NBTTagCompound nbt);
+    public boolean loadData(World world, CompoundNBT nbt);
 }
